1230f5d8f4e3f64697879ecab0badd0be226108d
Alex Ipgefer <alex.ipgefer@yandex.ru>
2021-11-06 23:26:15 +0300

    Formated code

--- ToDoList.js
+++ ToDoList.js
@@ -21,23 +21,23 @@ const showList = () => {
     let toDoTask = 'ToDo: \n';
     let inProgress = 'In progress: \n';
     let doneTask = 'Done: \n';
-      for (let key in list) {
-          switch (list[key]) {
+    for (let key in list) {
+        switch (list[key]) {
             case 'ToDo':
-        toDoTask += [key] + ',\n' ;
-        break;
-        case 'In progress':
-        inProgress += [key] + ',\n';
-        break;
-       case 'Done':
-        doneTask += [key] + ',\n';
-        break;
-      }
-      }
-      console.log(toDoTask, inProgress, doneTask);
-  };
-  addTask('drink water')
-  addTask('have a walk')
-  changeStatus('have a walk', 'Done')
-  changeStatus('drink water', 'In progress')
-  showList();
+                toDoTask += [key] + ',\n';
+                break;
+            case 'In progress':
+                inProgress += [key] + ',\n';
+                break;
+            case 'Done':
+                doneTask += [key] + ',\n';
+                break;
+        }
+    }
+    console.log(toDoTask, inProgress, doneTask);
+};
+addTask('drink water')
+addTask('have a walk')
+changeStatus('have a walk', 'Done')
+changeStatus('drink water', 'In progress')
+showList();
